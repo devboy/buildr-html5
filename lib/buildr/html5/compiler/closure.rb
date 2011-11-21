39315e90f55e9d9d32769e9f0dfbb79e6b031011
Dominic Graefen <dominic.graefen@gmail.com>
2011-11-21 18:02:21 +0100

    Adds additional artifacts needed by the closure compiler, so executing will now work

--- lib/buildr/html5/compiler/closure.rb
+++ lib/buildr/html5/compiler/closure.rb
@@ -7,7 +7,7 @@ module Buildr
                 :target_ext => "js",
                 :packaging => :js
 
-        COMPILE_OPTIONS = [:warnings, :debug]
+        COMPILE_OPTIONS = [:warnings, :debug, :output]
 
         def initialize(project, options)
           super
@@ -20,6 +20,7 @@ module Buildr
 
         # This is a constant with closure info, the version at the end should be made dynamic
         CLOSURE_SPEC = 'com.google.javascript:closure-compiler:jar:r1592'
+        DEPENDENCIES = [Buildr.artifact("args4j:args4j:jar:2.0.12"),Buildr.artifact("com.google.guava:guava:jar:r07")]
 
         def compile(sources, target, dependencies)
           check_options options, COMPILE_OPTIONS
@@ -28,9 +29,15 @@ module Buildr
           # We call invoke to start that process
           closure_jar = Buildr.artifact(CLOSURE_SPEC)
           closure_jar.invoke
+          DEPENDENCIES.map(&:invoke)
 
           # We create an array with arguments
-          cmd_args = [] << "-jar" << "#{closure_jar.to_s}"
+          cmd_args = [] << "-classpath" << (DEPENDENCIES + [closure_jar]).map(&:to_s).join(File::PATH_SEPARATOR) << "com.google.javascript.jscomp.CommandLineRunner"
+          # We add our source js file
+          cmd_args += generate_source_args sources
+          # We add our output file
+          cmd_args << "--js_output_file" << File.join( target, options[:output] || "#{@project.name}.js" )
+
 
           unless Buildr.application.options.dryrun
             Java::Commands.java cmd_args
@@ -40,6 +47,16 @@ module Buildr
 
         def needed?(sources, target, dependencies)
           true
+        end
+
+        protected
+
+        def generate_source_args sources
+          source_args = []
+          source_files = []
+          sources.each{ |source_dir| source_files += Dir.glob(File.join(source_dir,"**","*.js"))}
+          source_files.each{ |source_file| source_args << "--js" << source_file }
+          source_args
         end
 
       end
