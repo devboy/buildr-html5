54dc87e2bbe3d738d02e639983d027b4f5b21055
devboy <dominic.graefen@gmail.com>
2011-11-21 16:28:21 +0100

    adds add starting point for the closure compiler

--- lib/buildr/html5/compiler/closure.rb
+++ lib/buildr/html5/compiler/closure.rb
@@ -7,11 +7,38 @@ module Buildr
                 :target_ext => "js",
                 :packaging => :js
 
-        def compile(sources, target, dependencies)
+        COMPILE_OPTIONS = [:warnings, :debug]
+
+        def initialize(project, options)
           super
+          options[:debug] = Buildr.options.debug if options[:debug].nil?
+          options[:warnings] ||= true
         end
 
-        def needed?
+        # We need to add the maven central repository so buildr will find the closure compiler
+        Buildr.repositories.remote << "http://repo2.maven.org/maven2"
+
+        # This is a constant with closure info, the version at the end should be made dynamic
+        CLOSURE_SPEC = 'com.google.javascript:closure-compiler:jar:r1592'
+
+        def compile(sources, target, dependencies)
+          check_options options, COMPILE_OPTIONS
+          # Buildr will make an artifact task which takes care of finding it in the maven repo,
+          # and downloading it automatically
+          # We call invoke to start that process
+          closure_jar = Buildr.artifact(CLOSURE_SPEC)
+          closure_jar.invoke
+
+          # We create an array with arguments
+          cmd_args = [] << "-jar" << "#{closure_jar.to_s}"
+
+          unless Buildr.application.options.dryrun
+            Java::Commands.java cmd_args
+          end
+
+        end
+
+        def needed?(sources, target, dependencies)
           true
         end
 
